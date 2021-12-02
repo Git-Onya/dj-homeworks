e9d1e4c8d89da8b18ba153e74b32392a00a8535c
Anna Golovko <a.v.golovko@bk.ru>
2021-12-02 17:51:46 +0300

    fifth HW

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from django.contrib import admin
 from measurement.views import SensorView, SensorDetailView, MeasurementView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('sensors/', SensorView.as_view()),
     path('sensors/<int:sensor>/', SensorDetailView.as_view()),
     path('measurements/', MeasurementView.as_view()),
